b26b67f9ce274bd54a904ff6ac049c5efc7edd98
andbri321 <andbri321@gmail.com>
2020-07-12 12:45:09 -0300

    [update] config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,4 +32,13 @@ module OneBitContacts
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+
+  config.middleware.insert_before 0, Rack::Cors do  
+    allow do    
+      origins '*'    
+      resource '*',    
+        headers: :any,    
+        methods: %i(get post put patch delete options head)    
+    end  
+  end
 end

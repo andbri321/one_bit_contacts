a071d94410bd8bec686f7ccc2d0068aa2ebfc445
andbri321 <andbri321@gmail.com>
2020-07-13 21:57:08 -0300

    [u] contact_spec

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 describe "get all contacts route", :type => :request do
     let!(:contacts) {FactoryBot.create_list(:random_contacts, 20)}
-    before {get '/api/v1/contacts'}
-    let(:user) { FactoryBot.create(:user) }
 
-    # before(:each) do 
-    #     # user = FactoryBot.create(:user)
-    #     request.env["devise.mapping"] = Devise.mappings[:user]
-    #     request.headers['X-User-Email'] = "#{user.email}"
-    #     request.headers['X-User-Token'] = "#{user.authentication_token}"
-    #     login_as(user, :scope => :user)
-    # end
+    before(:each) do 
+        get '/api/v1/contacts'
+        user = FactoryBot.create(:user)
+        
+        request.env["devise.mapping"] = Devise.mappings[:user]
+        request.headers['X-User-Email'] = "#{user.email}"
+        request.headers['X-User-Token'] = "#{user.authentication_token}"
+        sign_in user
+    end
 
     it 'returns all contacts' do
-        expect(JSON.parse(response.body).size).to eq(20)
+        expect(response.body).to eq(20)
+        # expect(JSON.parse(response.body).size).to eq(20)
     end
     it 'returns status code 200' do
         expect(response).to have_http_status(:success)
